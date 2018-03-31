d2902cefee1ee1ce1a5567d8a454a5599e846c9d
Angel-kou <1844646307@qq.com>
2018-03-31 19:22:37 +0800

    statistics the count of same elemnts in collection1,particularly, the element in collection1 contains character -, kmj 2018/3/31 19:22

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,28 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        String key = "";
+        int value=0;
+        for (String s:collection1) {
+            key = s;
+            value = 1;
+            if(s.contains("-")){
+                key = s.substring(0,1);
+                value = Integer.parseInt(s.substring(2,s.length()));
+            }
+            if(map.keySet().contains(key)){
+                map.put(key,map.get(key)+1);
+            }else {
+                map.put(key,value);
+            }
+        }
+        return map;
     }
 }
