2d55cb1a7ede6c6881c018dc435ae469cc6090c9
Angel-kou <1844646307@qq.com>
2018-03-31 19:31:11 +0800

    find the key of map which equals the element of value attribute of the object,modify the value of key and return map , kmj 2018/3/31

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -2,11 +2,21 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        List<String> sameCharacter = collectionA.keySet().stream()
+                .filter(s->object.get("value").contains(s))
+                .collect(Collectors.toList());
+        for (String key:collectionA.keySet()) {
+            if (sameCharacter.contains(key)){
+                collectionA.put(key,collectionA.get(key)-1);
+            }
+        }
 
-        return null;
+        return collectionA;
     }
 }
