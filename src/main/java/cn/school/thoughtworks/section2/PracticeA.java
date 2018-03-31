0c9f48c40d6895fdfec1fa8ea9cebb29e5724c23
Angel-kou <1844646307@qq.com>
2018-03-31 19:19:58 +0800

    statistics the count of same elements in collection1 by kmj 2018/3/31 19:19

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,21 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> map = new HashMap<>();
+        for (String s:collection1) {
+            if(map.keySet().contains(s)){
+                map.put(s,map.get(s)+1);
+            }else {
+                map.put(s,1);
+            }
 
-        return null;
+        }
+        return map;
     }
 }
