f5cb298a0ab51bea5992949151821d2c861469df
Angel-kou <1844646307@qq.com>
2018-03-31 19:43:25 +0800

    the difference between PracticeC and PracticeD is the element of collection1 contains -,the process is the same , kmj 2018/3/31 19:43

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,30 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        PracticeB practiceB = new PracticeB();
+        Map<String, Integer> map = new HashMap<>();
+        String key = "";
+        int value=0;
+        for (String s:collectionA) {
+            key = s;
+            value = 1;
+            if(s.contains("-")){
+                key = s.substring(0,1);
+                value = Integer.parseInt(s.substring(2,s.length()));
+            }
+            if(map.keySet().contains(key)){
+                map.put(key,map.get(key)+1);
+            }else {
+                map.put(key,value);
+            }
+        }
+        map = practiceB.createUpdatedCollection(map,object);
+        return map;
     }
 }
