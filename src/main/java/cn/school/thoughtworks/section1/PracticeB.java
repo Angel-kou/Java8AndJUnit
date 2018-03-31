d6a60b3e84b59d1d67f4b3d4c0d0c3ccec877d75
Angel-kou <1844646307@qq.com>
2018-03-31 19:15:06 +0800

    choose same elements between collection1 and subarray of collection2 by kmj2018/3/31 19:32

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = collection1.stream()
+                .filter(s->collection2.stream().filter(list->list.contains(s)).count()!=0)
+                .collect(Collectors.toList());
+        return result;
     }
 }
