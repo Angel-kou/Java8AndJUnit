6110e0cbe2b6c1b463020fa882146694c7134dcc
Angel-kou <1844646307@qq.com>
2018-03-31 19:24:23 +0800

    statistics the count of same elemnts in collection1,particularly, the element in collection1 contains character -,:and [], kmj 2018/3/31 19:23

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,31 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        String key = "";
+        int value=0;
+        for (String s:collection1) {
+            key = s;
+            value = 1;
+            if(s.contains("-")||s.contains(":")||s.contains("[")){
+                key = s.substring(0,1);
+                if(!s.contains("[")){
+                    value = Integer.parseInt(s.substring(2,s.length()));
+                }else {
+                    value = Integer.parseInt(s.substring(2, s.length() - 1));
+                }
+            }
+            if(map.keySet().contains(key)){
+                map.put(key,map.get(key)+value);
+            }else {
+                map.put(key,value);
+            }
+        }
+        return map;
     }
 }
