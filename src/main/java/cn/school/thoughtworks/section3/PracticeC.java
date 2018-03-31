4b8a59af425b9c1865ff72b9809f8084e97e9f77
Angel-kou <1844646307@qq.com>
2018-03-31 19:39:15 +0800

    statistic the number of same element in collection1 , form a map, and exectute the process of PracticeB in section3 , kmj 2018/3/31 19:39

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,22 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        PracticeB practiceB = new PracticeB();
+        Map<String, Integer> map = new HashMap<>();
+        for (String s:collectionA) {
+            if(map.keySet().contains(s)){
+                map.put(s,map.get(s)+1);
+            }else {
+                map.put(s,1);
+            }
+        }
+        map =practiceB.createUpdatedCollection(map,object);
+        return map;
     }
 }
